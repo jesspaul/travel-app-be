a4eb8e69a7fe32a2ce1b7315f818ef5d6c91e66b
Jessica Paul <paul.jessica95@gmail.com>
2021-04-08 15:25:52 -0400

    fix npm package issue

--- routes/countries.js
+++ routes/countries.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.router();
+const router = express.Router();
 const countriesCtrl = require('../controllers/countries');
 
 router.get('/', countriesCtrl.index);
