cb9c654f770eadd931ea0c8d1c80436575313250
Jessica Paul <paul.jessica95@gmail.com>
2021-04-14 10:04:04 -0400

    add create city functionality

--- controllers/cities.js
+++ controllers/cities.js
@@ -11,6 +11,19 @@ async function index(req, res) {
     }
 }
 
+async function create(req, res) {
+    try {
+        const country = await Country.findById(req.query.countryId);
+        country.cities.push(req.body);
+        await country.save();
+        index(req, res);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ error: 'something went wrong' });
+    }
+}
+
 module.exports = {
     index,
+    create,
 }
--- routes/cities.js
+++ routes/cities.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const citiesCtrl = require('../controllers/cities');
 
 router.get('/', citiesCtrl.index);
-// router.post('/', citiesCtrl.create);
+router.post('/', citiesCtrl.create);
 // router.delete('/:id', citiesCtrl.delete);
 // router.put('/:id', citiesCtrl.update);
 
