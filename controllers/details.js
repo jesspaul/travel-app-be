e788724da28c7c94f967ea0770776c8f972c8288
Jessica Paul <paul.jessica95@gmail.com>
2021-04-15 11:15:30 -0400

    add detail update functionality

--- controllers/details.js
+++ controllers/details.js
@@ -41,8 +41,24 @@ async function deleteDetail(req, res) {
     }
 }
 
+async function update(req, res) {
+    try {
+        const country = await Country.findById(req.query.countryId);
+        let city = country.cities.id(req.query.cityId);
+        let editDetail = city.details.id(req.params.id);
+        editDetail.set(req.body);
+        await country.save();
+        req.query.uid = country.uid;
+        index(req, res);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ error: 'something went wrong'});
+    }
+}
+
 module.exports = {
     index,
     create,
-    delete: deleteDetail
+    delete: deleteDetail,
+    update
 }
--- routes/details.js
+++ routes/details.js
@@ -5,6 +5,6 @@ const detailsCtrl = require('../controllers/details');
 router.get('/', detailsCtrl.index);
 router.post('/', detailsCtrl.create);
 router.delete('/:id', detailsCtrl.delete);
-// router.put('/:id', detailsCtrl.update);
+router.put('/:id', detailsCtrl.update);
 
 module.exports = router;
